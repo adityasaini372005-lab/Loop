43ade6d8e8256a56a7ec4706efecd6d908481e61
Aditya Saini <adityasaini372005@gmail.com>
2026-03-14 09:19:34 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class loop {
+    
+    public static void main(String[] args) {
+        int number;
+        System.out.println("Enter number for multiplication table:");
+        Scanner sc=new Scanner(System.in);
+        number=sc.nextInt();
+        for(int i=1;i<=10;i++)
+            System.out.println(number*i);
+    }
+}       
